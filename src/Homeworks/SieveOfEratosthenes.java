6bbfd3a9ffa9c3c51ee4e38037f157a3ee961bd2
Onur Cura <onurcuraa@gmail.com>
2023-06-19 14:37:13 +0300

    First commit

--- src/Homeworks/SieveOfEratosthenes.java
+++ src/Homeworks/SieveOfEratosthenes.java
@@ -9,19 +9,19 @@ public class SieveOfEratosthenes {
         int num = scan.nextInt();
 
         //2'den aldığım sayıya kadar olan değerleri içeren bir array oluşturdum.
-        int a[]= new int[num-1];
-        for (int i =0;i<num-1;i++){
-            a[i] = i+2;
+        int a[] = new int[num - 1];
+        for (int i = 0; i < num - 1; i++) {
+            a[i] = i + 2;
         }
 
         //Sırasıyla n2>num olana kadar 2 ve katları, 3 ve katları diye devam eden indexleri 0 atadım.
         //Bunu yaparken daha önce 0 a atadıysam atlaması için if ile kontrol ettim.
-        int i =0;
-        while(a[i]*a[i]<=num){
-            int n =1;
-            if (a[i]!=0){
-                while(i+a[i]*n <=a.length){
-                    a[i+a[i]*n]=0;
+        int i = 0;
+        while (a[i] * a[i] <= num) {
+            int n = 1;
+            if (a[i] != 0) {
+                while (i + a[i] * n <= a.length) {
+                    a[i + a[i] * n] = 0;
                     n++;
                 }
             }
@@ -30,9 +30,9 @@ public class SieveOfEratosthenes {
 
         //Arrayimde 0'a eşit olmayan indexleri yazdırdım.
         System.out.print(num + " a kadar olan asal sayılar : ");
-        for (int k = 0 ; k<num-1;k++){
-            if (a[k]!=0){
-                System.out.print(a[k] +" ");
+        for (int k = 0; k < num - 1; k++) {
+            if (a[k] != 0) {
+                System.out.print(a[k] + " ");
             }
         }
     }
